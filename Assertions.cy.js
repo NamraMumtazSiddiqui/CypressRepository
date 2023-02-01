d400d678fd3c6b6ddf0cc974bca30731bcf2f287
NamraMumtazSiddiqui <mumtaznamra@gmail.com>
2023-02-01 20:23:51 +0500

    Seconf Commit

--- Assertions.cy.js
+++ Assertions.cy.js
@@ -5,7 +5,13 @@ describe("Assertions Practice", () => {
     it("Simple Assertion", () => {
       cy.visit("https://www.lonestar.edu/testing-cypress.htm");
       cy.contains('Schedule An Appointment').click()
-
+      cy.get('.nav-mobile-sm-hide > .cta-tracker')
+      .should('contain','Apply')
+      .should('have.class','cta-tracker btn btn-xs btn-nav-promo',{setTimeout:70000})
+      .should('be.visible')
+      .should('be.enabled',{setTimeout:70000})
+      cy.get('#cta-primary-about').invoke('attr','id').should('equal','cta-primary-about')
+     
 
 })
 })
